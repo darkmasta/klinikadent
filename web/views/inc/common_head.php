9b5333a10d784e944dd80b308a20e6419f340b65
darkmasta <alperen_bagci@hotmail.com>
2015-09-01 02:24:32 +0300

    Remove

--- web/views/inc/common_head.php
+++
@@ -1,2 +0,0 @@
-     <meta charset='utf-8'>
-		 <link href="css/main.css" type="text/css" rel="stylesheet" />
--- web/views/js/base.js
+++
@@ -1,7 +0,0 @@
-$('.mobileMenuIcon').on('click', function() {
-	
-	//$("html,body").scrollTop(0);
-	
-	$('.mobileMenu').slideToggle();
-	
-});
